219b6f36ea253cb0e1021bf19c3953eefaa6606a
Stefan Schwetschke <stefan@schwetschke.de>
2015-01-13 23:03:19 +0100

    Initial revision
    * [Feature] SSH VPN
    * [Feature] stores client and server keys in a volume
    * [Feature] regenerates prime numbers for key exchange

---
+++ Dockerfile
@@ -0,0 +1,53 @@
+FROM ubuntu:14.04
+MAINTAINER Stefan Schwetschke <stefan@schwetschke.de>
+
+ENV SSH_BASE_VERSION 2015-01-07
+
+RUN apt-get update && apt-get install -y openssh-server
+RUN mkdir /var/run/sshd
+
+ENV SSH_CONFIGURATION 2015-01-07
+
+RUN mkdir /ssh
+RUN mkdir /ssh.template
+
+# SSH login fix: Inside the container, the login id attribute cannot be set. 
+# Without the fix, user is kicked off after login
+RUN perl -pi -e 's/^\s*session\s+required\s+pam_loginuid(.*)$/session optional pam_loginuid$1/g' /etc/pam.d/sshd
+
+COPY sshd_config /etc/ssh/
+
+COPY banner /ssh.template/banner.template
+RUN chmod a=r /ssh.template/banner.template
+RUN cp /ssh.template/banner.template /ssh.template/banner
+
+# Space delimited list of VPN users. Usually only one user is needed
+ENV VPNUSERS vpn
+
+RUN for i in $VPNUSERS; do adduser --gecos "" --shell /bin/false --disabled-login --disabled-password --no-create-home  $i; done 
+COPY authorized_keys /ssh.template/authorized_keys.template
+RUN chmod a=r /ssh.template/authorized_keys.template
+RUN for i in $VPNUSERS; do cp -v -p /ssh.template/authorized_keys.template /ssh.template/authorized_keys_${i}; done
+
+
+# Generating new prime numbers for key exchange. The default numbers might be compromised.
+# This will take about an hour, even on a fast server!
+ENV SKIP_SERVER_PRIME_NUMBERS 0 
+
+# This should only be used for debug purposes
+ENV STORE_SERVER_KEYS_IN_CONTAINER 0
+
+# Update of the binaries stored in the image
+ENV SSH_UPDATE 2015-01-07
+
+RUN apt-get update
+RUN apt-get upgrade -y openssh-server
+
+COPY run-ssh.sh /
+RUN chmod +x /run-ssh.sh
+
+EXPOSE 22022
+VOLUME /ssh
+
+ENTRYPOINT ["/bin/bash", "/run-ssh.sh"]
+CMD ["/usr/sbin/sshd", "-De"]
---
+++ run-ssh.sh
@@ -0,0 +1,70 @@
+#! /bin/bash
+
+echo Always getting the newest security updates
+
+apt-get update
+apt-get upgrade -y openssh-server
+apt-get clean
+
+echo "Copying the templates"
+cp -v -p /ssh.template/*.template /ssh/
+cp -v -p -u /ssh.template/* /ssh/
+
+
+echo "Checking for stored prime numbers in volume"
+if test "${SKIP_SERVER_PRIME_NUMBERS}" != 1
+then
+  rm /etc/ssh/moduli || true
+  if test -f /ssh/moduli 
+  then
+    echo "Using stored prime numbers"
+  	cp -v -p /ssh/moduli /etc/ssh/moduli
+  else
+	echo "Updating prime numbers"
+	echo "This will take about an hour, even on a fast server"
+  	
+  	ssh-keygen -G /tmp/moduli -b 4096
+    ssh-keygen -T /etc/ssh/moduli -f /tmp/moduli
+  fi
+  cp -v -p -u /etc/ssh/moduli /ssh/
+else
+	echo "Use prime numbers delivered with OpenSSH. These might be compromised."
+fi
+
+echo "Updating server keys from container"
+
+if test "${STORE_SERVER_KEYS_IN_CONTAINER}" != 1 
+then
+    echo "Deleting host keys from container"
+	rm -f /etc/ssh/ssh_host_*key* || echo "No host keys found"
+else
+	echo "Using host keys stored in container. This should only be used for debug purpoeses."
+fi
+
+echo "Checking stored host keys in volume"
+cp -v -p /ssh/ssh_host_*key* /etc/ssh/ || echo "No server keys in container, creating new ones"
+
+if test ! -f /etc/ssh/ssh_host_ed25519_key
+then
+	ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N '' < /dev/null
+fi
+
+if test ! -f /etc/ssh/ssh_host_rsa_key
+then
+	ssh-keygen -t rsa -b 4096 -f /etc/ssh/ssh_host_rsa_key -N '' < /dev/null
+fi
+echo "Storing host keys in volume"
+cp -v -u -p /etc/ssh/ssh_host_*key* /ssh/
+
+echo Fixing permissions
+chown -R root:root /etc/ssh
+chmod -R a=rX /etc/ssh/
+chmod go= /etc/ssh/ssh_host_*key*
+
+chmod a-w /ssh
+
+ls -ld /etc/ssh /etc/ssh/ssh_host_*_key* /ssh /ssh/authorized_keys*
+
+echo Starting OpenSSH
+
+exec $@
